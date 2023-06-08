7e1a428057209adde8309440af5753c2ffbb8939
gauravvv-shukla <gauravvvshukla@gmail.com>
2023-06-09 03:47:38 +0530

    Favicon Icon fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,11 +5,16 @@ import Provider from '@components/Provider';
 export const metadata = {
     title : "Promptopia",
     description : 'Discover & Share AI Prompts'
+
 }
 
 const RootLayout = ({children}) => {
   return (
     <html lang='en'>
+      <head>
+          {/* <link rel="shortcut icon" href={favicon} /> */}
+          <link rel="shortcut icon" href='assets/images/logo.svg' />
+        </head>
         <body>
           <Provider>
             <div className='main'>
